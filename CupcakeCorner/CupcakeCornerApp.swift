91f9534ed0909c6ae8c6c150592df0535e5d90ec
Álvaro Gascón <alvarogascon@hotmail.com>
2024-05-30 08:44:07 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Álvaro Gascón on 30/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
